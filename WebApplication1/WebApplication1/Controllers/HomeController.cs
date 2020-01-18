e1e777fe8c604d9b7a99ce23fdcca81fcefe3923
Alex-made <alekcey94@gmail.com>
2020-01-18 23:05:28 +0300

    Запись в кота работает

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
+
 
 
 namespace WebApplication1.Controllers
@@ -14,11 +16,6 @@ namespace WebApplication1.Controllers
         
         public ActionResult Index()
         {
-            /*myConfiguration = new Configuration();
-            myConfiguration.Configure();
-            mySessionFactory = myConfiguration.BuildSessionFactory();
-            mySession = mySessionFactory.OpenSession();*/
-
             ISessionFactory sessionFactory =
             new Configuration().Configure().BuildSessionFactory();
 
@@ -30,15 +27,34 @@ namespace WebApplication1.Controllers
                 {
                     var princess = new Cat
                     {
-                        Name = "Princess",
-                        Sex = 'F',
-                        Weight = 7.4f
+                        Name = "Prince",
+                        Sex = 'M',
+                        Weight = 7.64f
                     };
 
-                    session.Save(princess);
-                    tx.Commit();
+                    //session.Save(princess);
+                    //tx.Commit();
+
+                    var task = new Task
+                    {
+                        Header = "Задача"
+                    };
 
                     
+                    /* task.Description = "Описание задачи";
+                    task.CompleteDate = DateTime.Now;
+                    task.Status = "Запланирована";
+                    task.Priority = 1;
+                   task.SubTasks.Add(new SubTask
+                                    {
+                                        Description = "Запланировать",
+                                        Status = "Выполнено"
+                                    });*/
+
+                    session.Save(task);
+                    tx.Commit();
+                    
+
                 }
 
                 using (var tx = session.BeginTransaction())
@@ -54,20 +70,7 @@ namespace WebApplication1.Controllers
                     tx.Commit();
                 }
 
-                using (var tx = session.BeginTransaction())
-                {
-                    var females = session
-                        .Query<Cat>()
-                        .Where(c => c.Sex == 'F')
-                        .ToList();
-
-                    
-
-                    ViewBag.Title = females.Select(x => x.Name).FirstOrDefault();
-
-
-                    tx.Commit();
-                }
+                
 
 
             }
--- WebApplication1/WebApplication1/Models/Task.cs
+++ WebApplication1/WebApplication1/Models/Task.cs
@@ -7,18 +7,25 @@ namespace WebApplication1.Models
 {
     public class Task
     {
-        public virtual int TaskId { get; set; }
+        public virtual string TaskId { get; set; }
 
-        public virtual string Name { get; set; }
+        public virtual string Header { get; set; }
 
-        public virtual string Description { get; set; }
+        //public virtual string Description { get; set; }
 
-        public virtual DateTime CompleteDate { get; set; }
+        //public virtual DateTime CompleteDate { get; set; }
 
-        public virtual string Status { get; set; }
+        //public virtual string Status { get; set; }
 
-        public virtual int Priority { get; set; }
+        //public virtual int Priority { get; set; }
 
+        //private IList<SubTask> _subTasks { get; set; }
+
+        //public virtual IList<SubTask> SubTasks
+        //{
+        //    get { return _subTasks ?? (_subTasks = new List<SubTask>()); }
+        //    set { _subTasks = value; }
+        //}
         
     }
 }
